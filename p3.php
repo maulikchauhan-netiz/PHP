3dcd936f20fb42d05c39e39928cafccc17f2eb00
maulik chauhan <maulik.rajput0001@gmail.com>
2026-06-24 18:04:24 +0530

    php code

---
+++ p3.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <?php
+        $a=10;
+        $b=2.5;
+        echo "value of a is : " ,$a;
+        echo "<br> this is php" . " this is esy to lern<br>";
+        print "value of b is : " .$b ."<br>";
+        $c=print $b;
+        print "<br> $c";
+        ?>
+        </body>
+        </html> 
