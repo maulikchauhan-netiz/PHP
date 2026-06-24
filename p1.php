3aa9d06b4f57a7f66f65b1eb55b43216120b75ba
maulik chauhan <maulik.rajput0001@gmail.com>
2026-06-24 17:52:16 +0530

    php code

---
+++ p1.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <h1> This is h1 </h1>
+        <p>This paragraph tag </p>
+        <?php
+        echo"this is php part";
+        ?>
+        <h2> This is h2 tag </h2>
+</body>
+</html>
