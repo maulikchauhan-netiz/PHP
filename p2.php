5e1d6f0911a721e9deaee96e4b0e23db922f0910
maulik chauhan <maulik.rajput0001@gmail.com>
2026-06-24 18:02:57 +0530

    php code

---
+++ p2.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <style>
+            .test{color:red ; font-size:50px ; text algn-"centre"}
+         </style>
+         </head>
+         <body>
+            <?php
+            echo "<p class='test'>This is internal css </p>";
+            echo "<p style ='color:blue ; font family:calibri'>This is inline ss </p>"
+            ?>
+            </body>
+            </html>
