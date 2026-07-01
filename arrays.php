b9bc5ff12cd1408efec0bc6e2c74f9bf1907205d
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-01 17:54:28 +0530

    php code

---
+++ arrays.php
@@ -0,0 +1,34 @@
+
+
+Skip to content
+Using Gmail with screen readers
+Conversations
+37% of 15 GB used
+Terms · Privacy · Program Policies
+Last account activity: 8 hours ago
+Details
+<?php
+$a=array("one","two","three");
+$b=array(10,"two",3);
+$c[0]=20;
+$c[1]="rajkot";
+$c[3]="true";
+for($i=0;$i<3;$i++)
+    {
+        echo $a[$i],"<br>";
+    }
+foreach ($b as $i) 
+    {
+      echo "    $i","<br>";
+    }
+for($i=0;$i<3;$i++)
+    {
+        echo $c[$i],"<br>";
+    } 
+    foreach ($c as $i) 
+    {
+      echo "    $i","<br>";
+    }    
+?>
+4.php
+Displaying 3.php.
