6046aa1020e47beec111506f17660d626981d59d
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-09 19:01:00 +0530

    php code

---
+++ UDF.php
@@ -0,0 +1,15 @@
+<?php
+function add($x,$y)
+{
+    $a=$x+$y;
+    return $a;  
+}
+#other method
+echo add(10,20);
+echo "<br>";    
+$a=20;
+$b=10;
+$d=add($a,$b);
+{
+    echo "addition is : ".$d;
+}
