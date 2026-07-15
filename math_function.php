9f9d7018a7ff5650cff56ee370c8b30ccec42f84
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-15 08:40:17 +0530

    php code

---
+++ math_function.php
@@ -0,0 +1,23 @@
+<?php
+    echo abs(4.5)."<br>";
+    echo abs(-4.5)."<br>";
+    echo ceil(4.3)."<br>";
+    echo floor(4.3)."<br>";
+    echo round(4.6)."<br>";
+    echo rand()."<br>";
+    echo rand(1,100)."<br>";
+    echo min(2,-5,1)."<br>";
+    echo max(2,5,1)."<br>";
+    echo pi()."<br>";
+    echo pow(2,3)."<br>";
+    echo sqrt(25)."<br>";
+    echo exp(2)."<br>";
+    echo log(3)."<br>";
+    echo decbin(3)."<br>";
+    echo decoct(30)."<br>";
+    echo dechex(10)."<br>";
+    echo sin(30)."<br>";
+    echo cos(30)."<br>";
+    echo tan(30)."<br>";
+
+?>    
