11e9e82c0bdab792e4aa1dfc0fd6ad410f53dff2
maulik chauhan <maulik.rajput0001@gmail.com>
2026-08-13 08:29:35 +0530

    php code

---
+++ validation.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <style>
+            .error{color: red;}
+        </style>
+    </head>
+    <body>
+        <?php
+        $n1 = $m1 = $a1 = $b1 = " ";
+        if(isset($_POST["sb"]))
+        {
+            if(is_numeric($_POST["uname"]))
+            {
+                $n1 = "username can't be numeric";
+            }
+            else
+            {
+                $a1 = "username is : ".$_POST["uname"];
+            }
+            if(stren($_POST["pass"]<10))
+            {
+                $m1 = "password must be of length ten";
+            }
+            else
+            {
+                $b1 = "password is : ".$_POST["pass"];
+            }  
+        }
+        
+        ?>
+        <form method = "post">
+            username:<input type = "text" name = "uname"><span class = "error">*<?php if(isset($n1))echo $n1;?></span><br><br>
+            password:<input type = "password" name = "pass"><span class = "error">*<?php if(isset($m1))echo $m1;?></span><br><br>
+            
+            <input type = "submit" name = "sb" value = "login">
+            <input type = "reset" name = "rs" value = "reset">
+</form>
+    </body>
+</html>
