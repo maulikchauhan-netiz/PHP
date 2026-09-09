e0df6105232a38c71e333502a5bcaac5e8160374
maulik chauhan <maulik.rajput0001@gmail.com>
2026-09-09 08:22:31 +0530

    php code

---
+++ hit counter/login.php
@@ -0,0 +1,22 @@
+<?php
+header("Cache-Control:no-store,no-cache,no-revalidate");
+?>
+<html>
+    <head>
+        <body>
+            <form method="POST">
+                Username:<input type="text" name="uname"><br>
+                Password:<input type="password" name="pass"><br>
+                <input type="submit" name="sb" value="Login"><br>
+            </form>
+            <?php
+            if (isset($_POST["sb"])) 
+                {
+                    session_start();
+                    $_SESSION["uname"] = $_POST["uname"];
+                    header("Location:home.php");
+                }     
+            ?>
+        </body>
+    </head>
+</html>
